d7de22ba2730eb4c5e5ef816580d376f99e0ae60
Rishi Lalwani <rishi.123.lalwani@gmail.com>
2026-03-05 19:56:41 +0530

    autoplay

--- app/components/movie/TrailerPlayer.tsx
+++ app/components/movie/TrailerPlayer.tsx
@@ -12,7 +12,7 @@ export function TrailerPlayer({ trailerId }: TrailerPlayerProps) {
       <iframe
         width="100%"
         height="100%"
-        src={`https://www.youtube.com/embed/${trailerId}?modestbranding=1&rel=0&iv_load_policy=3`}
+        src={`https://www.youtube.com/embed/${trailerId}?autoplay=1&mute=1&modestbranding=1&rel=0&iv_load_policy=3`}
         frameBorder="0"
         allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
