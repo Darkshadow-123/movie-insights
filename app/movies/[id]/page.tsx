218ff14cc24554c4b2600a0fa0d737a01f2eb3e6
Rishi Lalwani <rishi.123.lalwani@gmail.com>
2026-03-17 14:38:13 +0530

    .

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -11,8 +11,8 @@ import {
 import { Navbar } from "@/app/components/layout";
 
 async function getMovie(id: string): Promise<MovieData> {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ? process.env.NEXT_PUBLIC_BASE_URL : "";
-  const res = await fetch(`/api/movies/${id}`, { next:{revalidate:86400} });
+  const baseUrl = process.env.NODE_ENV === "production" ? "" : process.env.NEXT_PUBLIC_BASE_URL;
+  const res = await fetch(`${baseUrl}/api/movies/${id}`, { next:{revalidate:86400} });
 
   if (!res.ok) {
     if (res.status === 404) {
